ffd09cf9ca8cb3818ffddcf588dd5178d8d448c9
Sputnik <sputnik@outlook.com>
2024-09-06 16:04:01 +0800

    555 went wo gitignore, fixed

--- src/simple_follower_ros2/launch/adjust_hsv.launch.py
+++ src/simple_follower_ros2/launch/adjust_hsv.launch.py
@@ -1,23 +1,28 @@
 import os
+import launch_ros.actions
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import (DeclareLaunchArgument, GroupAction,
-                            IncludeLaunchDescription, SetEnvironmentVariable)
+from launch.actions import (DeclareLaunchArgument, IncludeLaunchDescription)
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition
+from launch.conditions import UnlessCondition
+from launch.actions import ExecuteProcess
 
-#def launch(launch_descriptor, argv):
 def generate_launch_description():
     bringup_dir = get_package_share_directory('turn_on_wheeltec_robot')
     launch_dir = os.path.join(bringup_dir, 'launch')
-    wheeltec_robot = IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(os.path.join(launch_dir, 'turn_on_wheeltec_robot.launch.py')),
-    )
 
     wheeltec_camera = IncludeLaunchDescription(
             PythonLaunchDescriptionSource(os.path.join(launch_dir, 'wheeltec_camera.launch.py')),
     )
-
     return LaunchDescription([
-        wheeltec_robot,wheeltec_camera,]
+        wheeltec_camera,
+        launch_ros.actions.Node(
+            package='simple_follower_ros2', 
+            executable='adjust_hsv', 
+            name='adjust_hsv',
+            )]
     )
 
